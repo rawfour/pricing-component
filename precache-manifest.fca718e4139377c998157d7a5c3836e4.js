805108b9117a9a43414b1aff6dcd75dae31fb1e0
rawfour <rafaldziedziela@outlook.com>
2020-05-08 01:25:39 +0200

    Updates

---
+++ precache-manifest.fca718e4139377c998157d7a5c3836e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb030a6387f0f9561b88eae8f42d7c54",
+    "url": "/pricing-component/index.html"
+  },
+  {
+    "revision": "203bd46d568ebe2ad048",
+    "url": "/pricing-component/static/js/2.21da5213.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pricing-component/static/js/2.21da5213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5beee1f274ff63deaee7",
+    "url": "/pricing-component/static/js/main.1d59d68b.chunk.js"
+  },
+  {
+    "revision": "9399c7525802ee0984e5",
+    "url": "/pricing-component/static/js/runtime-main.ab0ea895.js"
+  },
+  {
+    "revision": "e72dda74b6d264819d76272de9192daf",
+    "url": "/pricing-component/static/media/bg-bottom.e72dda74.svg"
+  },
+  {
+    "revision": "bdfbd58ada7e697e4eb44a0be45baff0",
+    "url": "/pricing-component/static/media/bg-top.bdfbd58a.svg"
+  }
+]);
